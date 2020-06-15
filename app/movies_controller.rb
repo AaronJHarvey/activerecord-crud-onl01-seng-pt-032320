df6461e11ba9292687a84e70859bd9be40e6119d
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-15 01:49:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,15 +35,15 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  Movies.first 
+  Movie.first 
 end
 
 def can_get_the_last_item_in_the_database
- Movies.last 
+ Movie.last 
 end
 
 def can_get_size_of_the_database
-  Movies.count
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
